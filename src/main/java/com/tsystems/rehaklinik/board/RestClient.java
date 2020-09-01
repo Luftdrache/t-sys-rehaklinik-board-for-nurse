dc90b755d9d54906d1aadef6ecbd24cb68c1f3f7
Julia <ydalskaya@gmail.com>
2020-09-01 15:12:40 +0300

    Exception handling and properties file added

--- src/main/java/com/tsystems/rehaklinik/board/RestClient.java
+++ src/main/java/com/tsystems/rehaklinik/board/RestClient.java
@@ -9,26 +9,43 @@ import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.MediaType;
-import java.io.Serializable;
+import java.io.*;
+import java.util.Collections;
 import java.util.List;
+import java.util.Properties;
 
 @ManagedBean
 public class RestClient implements Serializable {
 
-    private Logger logger = LoggerFactory.getLogger(RestClient.class);
-
-    private static final String REST_ENDPOINT = "http://localhost:8081/rehaklinik/api/today-treatment-events";
+    private transient Logger logger = LoggerFactory.getLogger(RestClient.class);
 
     public List<TreatmentEvent> getTodayTreatmentEvents() {
         logger.info("NURSE BOARD: RestClient: getting today treatment events");
-        Client client = ClientBuilder.newClient();
-        List<TreatmentEvent> treatmentEvents = client
-                .target(REST_ENDPOINT)
-                .request(MediaType.APPLICATION_JSON)
-                .get(new GenericType<List<TreatmentEvent>>() {
-                });
-        client.close();
-        logger.info("NURSE BOARD: RestClient: data received");
-        return treatmentEvents;
+
+        Properties properties = new Properties();
+
+        try (InputStream input = getClass().getClassLoader().getResourceAsStream("rest.properties")) {
+
+            properties.load(input);
+            Client client = ClientBuilder.newClient();
+            List<TreatmentEvent> treatmentEvents = client
+                    .target(properties.getProperty("rest.endpoint"))
+                    .request(MediaType.APPLICATION_JSON)
+                    .get(new GenericType<List<TreatmentEvent>>() {
+                    });
+            client.close();
+
+            logger.info("NURSE BOARD: RestClient: data received");
+
+            return treatmentEvents;
+
+        } catch (FileNotFoundException exception) {
+            logger.error("NURSE BOARD: RestClient: could not find properties file", exception.fillInStackTrace());
+        } catch (IOException exception) {
+            logger.error("NURSE BOARD: RestClient: IOException thrown", exception.fillInStackTrace());
+        } catch (Exception exception) {
+            logger.error("NURSE BOARD: RestClient: could not connect to remote server");
+        }
+        return Collections.emptyList();
     }
 }
